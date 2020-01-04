14d7d75d99d0f9f3046b5e620df0ee92ec993343
Usagi Ito <the@usagi.network>
2020-01-04 20:19:55 +0900

    fixed log message bugs; no changes for behaviors

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -9,37 +9,37 @@ $FILES = @(
   'vcpkg_chii_find_package.cmake'
 )
 
-Write-Output 'vcpkg_chii/bootstrap.sh: [ LOG ] Congraturations, the bootstrap of vcpkg_chii be started.'
+Write-Output 'vcpkg_chii/bootstrap.ps1: [ LOG ] Congraturations, the bootstrap of vcpkg_chii be started.'
 
 if (!( Test-Path cmake) ) {
   New-Item cmake -ItemType Directory
   if ( !$? -or !( Test-Path cmake) ) {
-    Write-Output 'vcpkg_chii/bootstrap.sh: [ FATAL ] Could not create the cmake directory.'
+    Write-Output 'vcpkg_chii/bootstrap.ps1: [ FATAL ] Could not create the cmake directory.'
     exit 2
   }
-  Write-Output 'vcpkg_chii/bootstrap.sh: [ LOG ] `New-Item cmake -ItemType Directory` was succeeded.'
+  Write-Output 'vcpkg_chii/bootstrap.ps1: [ LOG ] `New-Item cmake -ItemType Directory` was succeeded.'
 }
 
 Set-Location cmake
 if ( ! $? ) {
-  Write-Output 'vcpkg_chii/bootstrap.sh: [ FATAL ] `Set-Location cmake` was failed.'
+  Write-Output 'vcpkg_chii/bootstrap.ps1: [ FATAL ] `Set-Location cmake` was failed.'
   exit 3
 }
-Write-Output 'vcpkg_chii/bootstrap.sh: [ LOG ] `Set-Location cmake` was succeeded.'
+Write-Output 'vcpkg_chii/bootstrap.ps1: [ LOG ] `Set-Location cmake` was succeeded.'
 
 $COUNT_OF_FILES = $FILES.Length
 
-Write-Output "vcpkg_chii/bootstrap.sh: [ LOG ] Begin download the ${COUNT_OF_FILES} .cmake files..."
+Write-Output "vcpkg_chii/bootstrap.ps1: [ LOG ] Begin download the ${COUNT_OF_FILES} .cmake files..."
 
 foreach ( $FILE in $FILES ) {
   $URI = $BASE + $FILE
-  Write-Output "vcpkg_chii/bootstrap.sh: [ LOG ] Downloading... $URI"
+  Write-Output "vcpkg_chii/bootstrap.ps1: [ LOG ] Downloading... $URI"
   Invoke-WebRequest -Uri $URI -OutFile $FILE
   if ( ! $? ) {
-    Write-Output 'vcpkg_chii/bootstrap.sh: [ FATAL ] `Invoke-WebRequest` was failed.'
+    Write-Output 'vcpkg_chii/bootstrap.ps1: [ FATAL ] `Invoke-WebRequest` was failed.'
     exit 3
   }
 }
 
-Write-Output 'vcpkg_chii/bootstrap.sh: [ LOG ] Completed, you can use `include(cmake/vcpkg_chii.cmake)` in your CMakeLists.txt and use vcpkg_chii features!'
-Write-Output 'vcpkg_chii/bootstrap.sh: [ LOG ] ( See also the example <https://github.com/usagi/vcpkg_chii/blob/master/example/CMakeLists.txt#L1>. )'
+Write-Output 'vcpkg_chii/bootstrap.ps1: [ LOG ] Completed, you can use `include(cmake/vcpkg_chii.cmake)` in your CMakeLists.txt and use vcpkg_chii features!'
+Write-Output 'vcpkg_chii/bootstrap.ps1: [ LOG ] ( See also the example <https://github.com/usagi/vcpkg_chii/blob/master/example/CMakeLists.txt#L1>. )'
